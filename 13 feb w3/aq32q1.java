7d55eed202cc6d225288b2a9b849683ce6a35ef2
Kanchi Vaishnavi <96121334+KanchiVaishnavi@users.noreply.github.com>
2023-02-14 06:49:36 +0000

    .

---
+++ 13 feb w3/aq32q1.java
@@ -0,0 +1,15 @@
+class India{
+        public India(){
+              System.out.println("Visit Northeast India.");
+        }
+}
+class Northeast extends India{
+        public Northeast(){        
+              System.out.println("Kolkata connects Manipur, Assam, Nagaland and Tripura directly by air." );                
+        }
+}
+public class aq32q1{
+        public static void main(String[] args){
+              Northeast charming = new Northeast();   
+        }
+}
